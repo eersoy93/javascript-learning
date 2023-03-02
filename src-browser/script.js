8689fa47c63093929952b1322173602089e2007d
Erdem Ersoy <erdemersoy256@gmail.com>
2023-03-02 11:48:19 +0300

    Add initial src-browser example
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

---
+++ src-browser/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function buttonClicked() {
+    document.getElementById("headerMain").innerHTML = "DON'T CLICK!!!";
+    document.getElementById("paragraphMain").innerHTML = "DON'T CLICK!!!";
+}
